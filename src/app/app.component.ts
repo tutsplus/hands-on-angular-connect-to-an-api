59d936a09618c8ed14cff4b3d73b648d1854a018
Dan Wellman <danwellman@hotmail.com>
2018-12-05 19:35:52 +0000

    Starter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hands-on-http';
+  public products: Array<any> = [];
+  public productForm: FormGroup;
+  public showForm: boolean;
+
+  constructor(
+    private readonly fb: FormBuilder,
+  ) {}
+
+  public get currentYear(): number {
+    return new Date().getFullYear();
+  }
+
+  public ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: [''],
+      stock: [''],
+      price: ['']
+    });
+  }
+
+  public onSubmit(): void {
+  }
 }
